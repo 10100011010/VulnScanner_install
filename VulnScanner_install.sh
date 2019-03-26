4699467f2408608c39d72dc8b5a08d52f34bfdfc
10100011010 <48951683+10100011010@users.noreply.github.com>
2019-03-26 16:21:58 +0200

    Add files via upload

---
+++ VulnScanner_install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Install a2sv
+echo 'Installing a2sv...'
+git clone https://github.com/hahwul/a2sv.git
+cd a2sv
+./install.sh
+cd ..
+
+# Install nmap-vulners
+echo 'Installing nmap-vulners...'
+git clone https://github.com/vulnersCom/nmap-vulners.git
+cd nmap-vulners
+cp vulners.nse /usr/share/nmap/scripts/
+
+# Create vulnscanner command
+echo 'Creating vulscanner command...'
+git clone https://github.com/10100011010/VulnScanner_Bash.git
+cd VulnScanner_Bash
+chmod +x vulnscanner
+cp vulnscanner /bin/vulnscanner
+
+# Inform the user
+echo -e '\nAll done!\n'
+echo -e 'Use:\nvulnscanner <domain>'
